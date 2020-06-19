f3174082ad0c382faf522e44e2cd00dc33d6bbfc
Filiberto Reyes <filibertoreyes55@gmail.com>
2020-06-19 13:19:45 -0700

    import py simple layout

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 
@@ -20,8 +21,39 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+Base = declarative_base()
+Base.query = db.query_property()
+
+
+# class Book(db.model):
+#     __tablename__ = 'books'
+#     id = db.Column(db.Integer, primary_key=True)
+#     isbn = db.Column(db.Integer, unique=True)
+#     title = db.Column(db.String)
+#     author = db.Column(db.String)
+#     year = db.Column(db.Integer)
+
+#     def __init__(self, isbn, title, author, year):
+#         self.isbn = isbn
+#         self.title = title
+#         self.author = author
+#         self.year = year
+
+
+# class User(db.model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String)
+#     password = db.Column(db.String)
+
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+
 
 # ROUTES
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -30,3 +62,17 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        return render_template('success.html')
